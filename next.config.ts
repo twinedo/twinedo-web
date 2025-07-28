2a09f2a31596ad31b9e6cbdc44fad9a4b08d50d0
Twin Edo <twinedo.dev@gmail.com>
2025-07-29 00:30:50 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,19 @@ import path from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  target: "serverless",
   serverRuntimeConfig: {
     PROJECTS_UPLOAD_DIR: path.join(process.cwd(), 'src', 'backend', 'src', 'uploads', 'projects'),
     CV_UPLOAD_DIR: path.join(process.cwd(), 'src', 'backend', 'src', 'uploads', 'cv')
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
